de2c1f0f37b5aab9d2653d4001ec045235444352
ffivaz <_Beck1640>
2019-09-09 16:02:19 +0200

    Adaptation code pour cartes OpenLayers

--- src/pages/first-insert-map/first-insert-map.page.ts
+++ src/pages/first-insert-map/first-insert-map.page.ts
@@ -1,4 +1,4 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AlertController, NavController} from 'ionic-angular';
 import {Geolocation} from '@ionic-native/geolocation';
 import proj4 from 'proj4';
@@ -17,7 +17,7 @@ import ol from "openlayers";
 export class FirstInsertMapPage implements OnInit {
     map: any;
     searching: boolean = false;
-    zoom: number = 18;
+    zoom: number = 16;
     observation: SimpleObservationModel = {
         rowid: null,
         StatusCode: null,
@@ -75,7 +75,6 @@ export class FirstInsertMapPage implements OnInit {
                 private n: Network,
                 private ac: AlertController,
                 private ts: TranslateService,
-                private z: NgZone,
                 private s: Storage) {
     }
 
@@ -111,13 +110,10 @@ export class FirstInsertMapPage implements OnInit {
 
     goToCurrentLocation() {
         this.searching = true;
-        /*this.gl.getCurrentPosition({'enableHighAccuracy': true})
+        this.gl.getCurrentPosition({'enableHighAccuracy': true})
             .then(position => {
-                let googlePosition: LatLng = new LatLng(position.coords.latitude, position.coords.longitude);
-                this.map.moveCamera({
-                    target: googlePosition
-                });
                 let swissCoords = proj4('EPSG:4326', this.swissProjection, [position.coords.longitude, position.coords.latitude]);
+                this.map.getView().setCenter(ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]));
                 this.currentLoc.X = Math.round(swissCoords[0]);
                 this.currentLoc.Y = Math.round(swissCoords[1]);
                 this.currentLoc.pr = Math.round(position.coords.accuracy);
@@ -126,27 +122,17 @@ export class FirstInsertMapPage implements OnInit {
                 this.observation.precisionCode = this.whatPrecision(Math.round(position.coords.accuracy));
                 this.searching = false;
                 this.s.set('lastcoords', [this.currentLoc.X, this.currentLoc.Y]);
-            });*/
+            });
     }
 
     backToInsert() {
-        /*let wgsCoords = proj4('EPSG:4326', this.swissProjection, [this.map.getCameraPosition().target.lng, this.map.getCameraPosition().target.lat]);
+        let wgsCoords = proj4('EPSG:4326', this.swissProjection, [this.map.getView().getCenter()[0], this.map.getView().getCenter()[1]]);
         this.observation.swissCoordinatesX = Math.round(wgsCoords[0]);
-        this.observation.swissCoordinatesY = Math.round(wgsCoords[1]);*/
+        this.observation.swissCoordinatesY = Math.round(wgsCoords[1]);
         this.nc.setRoot(InsertFormPage, {
             'obs': this.observation,
             'fix': true
         });
-    }
-
-    switchMapType() {
-        /*if (this.mapType == 'SATELLITE') {
-            this.mapType = 'ROADMAP'
-        } else {
-            this.mapType = 'SATELLITE'
-        }
-
-        this.map.setMapTypeId(GoogleMapsMapTypeId[this.mapType]);*/
     }
 
     backToHome() {
@@ -162,7 +148,6 @@ export class FirstInsertMapPage implements OnInit {
 
         this.s.get('lastcoords')
             .then((r) => {
-                console.log(r);
                 this.currentLoc.X = r[0];
                 this.currentLoc.Y = r[1];
                 this.currentLoc.pr = null;
@@ -170,74 +155,46 @@ export class FirstInsertMapPage implements OnInit {
                 this.currentLoc.pralti = null;
                 this.observation.precisionCode = null;
 
-                let wgsCoords = proj4(this.swissProjection, 'EPSG:4326', [this.currentLoc.X, this.currentLoc.Y]);
-
-                //let googlePosition: LatLng = new LatLng(wgsCoords[1], wgsCoords[0]);
-
-                /*let mapOptions: GoogleMapOptions = {
-                    mapType: GoogleMapsMapTypeId.SATELLITE,
-
-                    camera: {
-                        target: googlePosition,
-                        zoom: this.zoom,
-                        tilt: 0
-                    },
-
-                    controls: {
-                        'compass': false,
-                        'myLocationButton': false,
-                        'myLocation': false,
-                        'indoorPicker': false,
-                        'zoom': false,
-                        'mapToolbar': true
-                    }
-                };*/
+                let wgsCoords = proj4(this.swissProjection, 'EPSG:3857', [this.currentLoc.X, this.currentLoc.Y]);
 
                 this.map = new ol.Map({
-                    layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
+                    layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
                     target: document.getElementById('olmap'),
                     view: new ol.View({
-                        center: ol.proj.transform([-0.12755, 51.507222], 'EPSG:4326', 'EPSG:3857'),
-                        zoom: 3
+                        center: wgsCoords,
+                        zoom: this.zoom
                     })
                 });
 
-                /*this.map.one(GoogleMapsEvent.MAP_READY)
-                    .then(() => {
-                        this.gl.getCurrentPosition({'enableHighAccuracy': true})
-                            .then(position => {
-                                let swissCoords = proj4('EPSG:4326', this.swissProjection, [position.coords.longitude, position.coords.latitude]);
-                                let googlePosition: LatLng = new LatLng(position.coords.latitude, position.coords.longitude);
-                                this.map.moveCamera({
-                                    target: googlePosition
-                                });
-                                this.currentLoc.X = Math.round(swissCoords[0]);
-                                this.currentLoc.Y = Math.round(swissCoords[1]);
-                                this.currentLoc.pr = Math.round(position.coords.accuracy);
-                                this.currentLoc.alti = Math.round(position.coords.altitude);
-                                this.currentLoc.pralti = Math.round(position.coords.altitudeAccuracy);
-                                this.observation.precisionCode = this.whatPrecision(Math.round(position.coords.accuracy));
-                                this.searching = false;
-                                this.s.set('lastcoords', [this.currentLoc.X, this.currentLoc.Y]);
-                            });
+                this.gl.getCurrentPosition({'enableHighAccuracy': true})
+                    .then(position => {
+                        let swissCoords = proj4('EPSG:4326', this.swissProjection, [position.coords.longitude, position.coords.latitude]);
+                        this.map.getView().setCenter(ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]));
+                        this.currentLoc.X = Math.round(swissCoords[0]);
+                        this.currentLoc.Y = Math.round(swissCoords[1]);
+                        this.currentLoc.pr = Math.round(position.coords.accuracy);
+                        this.currentLoc.alti = Math.round(position.coords.altitude);
+                        this.currentLoc.pralti = Math.round(position.coords.altitudeAccuracy);
+                        this.observation.precisionCode = this.whatPrecision(Math.round(position.coords.accuracy));
+                        this.searching = false;
+                        this.s.set('lastcoords', [this.currentLoc.X, this.currentLoc.Y]);
                     });
 
-                this.map.on(GoogleMapsEvent.MAP_DRAG_END)
-                    .subscribe(() => {
-                        let wgsCoords = proj4('EPSG:4326', this.swissProjection, [this.map.getCameraPosition().target.lng, this.map.getCameraPosition().target.lat]);
-                        this.z.run(() => {
-                            this.observation.swissCoordinatesX = Math.round(wgsCoords[0]);
-                            this.observation.swissCoordinatesY = Math.round(wgsCoords[1]);
-                            this.currentLoc.X = Math.round(wgsCoords[0]);
-                            this.currentLoc.Y = Math.round(wgsCoords[1]);
-                            this.currentLoc.pr = null;
-                            this.currentLoc.alti = null;
-                            this.currentLoc.pralti = null;
-                            this.observation.precisionCode = null;
-                        });
-                    });*/
-            });
+                let that = this;
 
+                this.map.on('moveend', function () {
+                    console.log(proj4('EPSG:3857', that.swissProjection, that.map.getView().getCenter()));
+                    let wgsCoords = proj4('EPSG:3857', that.swissProjection, that.map.getView().getCenter());
+                    that.observation.swissCoordinatesX = Math.round(wgsCoords[0]);
+                    that.observation.swissCoordinatesY = Math.round(wgsCoords[1]);
+                    that.currentLoc.X = Math.round(wgsCoords[0]);
+                    that.currentLoc.Y = Math.round(wgsCoords[1]);
+                    that.currentLoc.pr = null;
+                    that.currentLoc.alti = null;
+                    that.currentLoc.pralti = null;
+                    that.observation.precisionCode = null;
+                });
+            });
     }
 
     ngOnInit() {
@@ -249,7 +206,8 @@ export class FirstInsertMapPage implements OnInit {
                 buttons: [
                     {
                         text: this.ts.instant('FERMER'),
-                        handler: () => {}
+                        handler: () => {
+                        }
                     }
                 ]
             });
--- src/pages/insert-map/insert-map.page.ts
+++ src/pages/insert-map/insert-map.page.ts
@@ -4,7 +4,7 @@ import proj4 from 'proj4';
 import {InsertFormPage} from "../insert-form/insert-form.page";
 import {SimpleObservationModel} from "../../models/simple-observation.model";
 import {HomeListPage} from "../home-list/home-list.page";
-import {Component, NgZone, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import ol from "openlayers";
 
 @Component({
@@ -14,7 +14,7 @@ import ol from "openlayers";
 export class InsertMapPage implements OnInit {
 
     map: any;
-    zoom: number = 18;
+    zoom: number = 16;
     swissProjection = '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs';
     currentLoc = {
         X: null,
@@ -29,8 +29,7 @@ export class InsertMapPage implements OnInit {
 
     constructor(private nc: NavController,
                 private gl: Geolocation,
-                private np: NavParams,
-                private z: NgZone) {
+                private np: NavParams) {
     }
 
     whatPrecision(accuracy): string {
@@ -65,15 +64,10 @@ export class InsertMapPage implements OnInit {
 
     goToCurrentLocation() {
         this.searching = true;
-        /*this.gl.getCurrentPosition({'enableHighAccuracy': true})
+        this.gl.getCurrentPosition({'enableHighAccuracy': true})
             .then(position => {
-                let googlePosition: LatLng = new LatLng(position.coords.latitude, position.coords.longitude);
-                this.map.moveCamera({
-                    target: googlePosition
-                });
                 let swissCoords = proj4('EPSG:4326', this.swissProjection, [position.coords.longitude, position.coords.latitude]);
-                this.observation.swissCoordinatesX = Math.round(swissCoords[0]);
-                this.observation.swissCoordinatesY = Math.round(swissCoords[1]);
+                this.map.getView().setCenter(ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]));
                 this.currentLoc.X = Math.round(swissCoords[0]);
                 this.currentLoc.Y = Math.round(swissCoords[1]);
                 this.currentLoc.pr = Math.round(position.coords.accuracy);
@@ -81,17 +75,7 @@ export class InsertMapPage implements OnInit {
                 this.currentLoc.pralti = Math.round(position.coords.altitudeAccuracy);
                 this.observation.precisionCode = this.whatPrecision(Math.round(position.coords.accuracy));
                 this.searching = false;
-            });*/
-    }
-
-    switchMapType() {
-        /*if (this.mapType == 'SATELLITE') {
-            this.mapType = 'ROADMAP'
-        } else {
-            this.mapType = 'SATELLITE'
-        }
-
-        this.map.setMapTypeId(GoogleMapsMapTypeId[this.mapType]);*/
+            });
     }
 
     backToInsert() {
@@ -111,57 +95,49 @@ export class InsertMapPage implements OnInit {
     loadMap() {
         this.searching = true;
 
-        let wgsCoords = proj4(this.swissProjection, 'EPSG:4326', [this.observation.swissCoordinatesX, this.observation.swissCoordinatesY]);
+        let wgsCoords = proj4(this.swissProjection, 'EPSG:3857', [this.observation.swissCoordinatesX, this.observation.swissCoordinatesY]);
 
         this.currentLoc.X = this.observation.swissCoordinatesX;
         this.currentLoc.Y = this.observation.swissCoordinatesY;
-
-        /*let googlePosition: LatLng = new LatLng(wgsCoords[1], wgsCoords[0]);
-
-        let mapOptions: GoogleMapOptions = {
-            mapType: GoogleMapsMapTypeId.SATELLITE,
-
-            camera: {
-                target: googlePosition,
-                zoom: this.zoom,
-                tilt: 0
-            },
-
-            controls: {
-                'compass': false,
-                'myLocationButton': false,
-                'myLocation': false,
-                'indoorPicker': false,
-                'zoom': false,
-                'mapToolbar': true
-            }
-        };*/
 
         this.map = new ol.Map({
             layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
             target: document.getElementById('olmap'),
             view: new ol.View({
-                center: ol.proj.transform([-0.12755, 51.507222], 'EPSG:4326', 'EPSG:3857'),
-                zoom: 3
+                center: wgsCoords,
+                zoom: this.zoom
             })
         });
 
-        /*this.map.on(GoogleMapsEvent.MAP_DRAG_END)
-            .subscribe(() => {
-                let wgsCoords = proj4('EPSG:4326', this.swissProjection, [this.map.getCameraPosition().target.lng, this.map.getCameraPosition().target.lat]);
-                this.z.run(() => {
-                    this.observation.swissCoordinatesX = Math.round(wgsCoords[0]);
-                    this.observation.swissCoordinatesY = Math.round(wgsCoords[1]);
-                    this.currentLoc.X = Math.round(wgsCoords[0]);
-                    this.currentLoc.Y = Math.round(wgsCoords[1]);
-                    this.currentLoc.pr = null;
-                    this.currentLoc.alti = null;
-                    this.currentLoc.pralti = null;
-                    this.observation.precisionCode = null;
-                });
+        this.gl.getCurrentPosition({'enableHighAccuracy': true})
+            .then(position => {
+                let swissCoords = proj4('EPSG:4326', this.swissProjection, [position.coords.longitude, position.coords.latitude]);
+                this.map.getView().setCenter(ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]));
+                this.currentLoc.X = Math.round(swissCoords[0]);
+                this.currentLoc.Y = Math.round(swissCoords[1]);
+                this.currentLoc.pr = Math.round(position.coords.accuracy);
+                this.currentLoc.alti = Math.round(position.coords.altitude);
+                this.currentLoc.pralti = Math.round(position.coords.altitudeAccuracy);
+                this.observation.precisionCode = this.whatPrecision(Math.round(position.coords.accuracy));
+                this.searching = false;
             });
 
-        this.searching = false;*/
+        let that = this;
+
+        this.map.on('moveend', function () {
+            console.log(proj4('EPSG:3857', that.swissProjection, that.map.getView().getCenter()));
+            let wgsCoords = proj4('EPSG:3857', that.swissProjection, that.map.getView().getCenter());
+            that.observation.swissCoordinatesX = Math.round(wgsCoords[0]);
+            that.observation.swissCoordinatesY = Math.round(wgsCoords[1]);
+            that.currentLoc.X = Math.round(wgsCoords[0]);
+            that.currentLoc.Y = Math.round(wgsCoords[1]);
+            that.currentLoc.pr = null;
+            that.currentLoc.alti = null;
+            that.currentLoc.pralti = null;
+            that.observation.precisionCode = null;
+        });
+
+        this.searching = false;
 
     }
 
