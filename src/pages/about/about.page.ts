3d5f94631bc90fa8d5f1d9376d568c6587929ff4
ffivaz <fabien.fivaz@unine.ch>
2019-09-09 23:47:43 +0200

    Documentations minimale (page à propos)

--- src/pages/about/about.page.ts
+++ src/pages/about/about.page.ts
@@ -1,3 +1,5 @@
+/** Page "A propos" accessible depuis le menu. Le texte est un fichier html dans le répertoire /src/assets/i18n/about.*lang*.html */
+
 import {Component, OnInit} from '@angular/core';
 import {NavController} from "ionic-angular";
 import {HomeListPage} from "../home-list/home-list.page";
@@ -5,47 +7,49 @@ import {TranslateService} from "@ngx-translate/core";
 import {Http} from "@angular/http";
 import {AppVersion} from "@ionic-native/app-version";
 
-
 @Component({
-    selector: 'page-about',
-    templateUrl: 'about.page.html'
+  selector: 'page-about',
+  templateUrl: 'about.page.html'
 })
 export class AboutPage implements OnInit {
 
-    lang: string;
-    text: any;
-    version: string[] = [];
+  lang: string;
+  text: any;
+  version: string[] = [];
 
-    constructor(
-        private nc: NavController,
-        private ts: TranslateService,
-        private http: Http,
-        private av: AppVersion
-    ) {}
+  constructor(
+    private nc: NavController,
+    private ts: TranslateService,
+    private http: Http,
+    private av: AppVersion
+  ) {
+  }
 
-    backToHome() {
-        this.nc.setRoot(HomeListPage);
-    }
+  backToHome() {
+    this.nc.setRoot(HomeListPage);
+  }
 
-    ngOnInit() {
-        this.lang = this.ts.currentLang;
+  ngOnInit() {
+    this.lang = this.ts.currentLang;
 
-        let fileToLoad = 'assets/i18n/about.' + this.lang + '.html';
+    let fileToLoad = 'assets/i18n/about.' + this.lang + '.html';
 
-        this.http.get(fileToLoad)
-            .subscribe((r)=>{
-                this.text = r.text();
-            });
+    this.http.get(fileToLoad)
+      .subscribe((r) => {
+        this.text = r.text();
+      });
 
-        this.av.getVersionNumber()
-            .then((r)=>{
-                this.version.push(r);
-            });
+    /** Le numéro de version (et de build) est extrait de /config.xml par le plugin ionic @ionic-native/app-version */
 
-        this.av.getVersionCode()
-            .then((r)=>{
-                this.version.push(String(r));
-            });
-    }
+    this.av.getVersionNumber()
+      .then((r) => {
+        this.version.push(r);
+      });
+
+    this.av.getVersionCode()
+      .then((r) => {
+        this.version.push(String(r));
+      });
+  }
 
 }
