18fb83b8ffa42fa805e2786e4e1ec72bacb2509c
ffivaz <fabien.fivaz@unine.ch>
2019-09-11 08:50:31 +0200

    Documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,166 +19,190 @@ import {FirstSpeciesListPage} from "../pages/first-species-list/first-species-li
 import {SpeciesService} from "../services/species.service";
 
 @Component({
-    templateUrl: 'app.html'
+  templateUrl: 'app.html'
 })
 export class MyApp {
-    @ViewChild(Nav) nav: Nav;
-    rootPage: any;
-    pages: Array<{ title: string, component: any }>;
-    onResumeObs: SimpleObservationModel;
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any;
+  pages: Array<{ title: string, component: any }>;
+  onResumeObs: SimpleObservationModel;
 
-    constructor(private platform: Platform,
-                private statusBar: StatusBar,
-                private splashScreen: SplashScreen,
-                private ts: TranslateService,
-                private os: ObservationsService,
-                private ss: SpeciesService,
-                private as: AuthService,
-                private s: Storage,
-                private http: HTTP,
-                private n: Network) {
-        this.initializeApp();
+  constructor(private platform: Platform,
+              private statusBar: StatusBar,
+              private splashScreen: SplashScreen,
+              private ts: TranslateService,
+              private os: ObservationsService,
+              private ss: SpeciesService,
+              private as: AuthService,
+              private s: Storage,
+              private http: HTTP,
+              private n: Network) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+
+    /**
+     * Au moment de prendre une photo, certains téléphones android ferme l'application afin d'économiser de la mémoire.
+     * Le code ci-dessous permet de récupérer l'image et de retourner directement au formulaire de saisie lors
+     * de la réouverture de l'application.
+      */
+    if (this.platform.is('android')) {
+      this.platform.resume.subscribe((event: any) => {
+        if (event && event.pendingResult.pluginServiceName == 'Camera' && event.pendingResult.pluginStatus == 'OK') {
+          this.s.get('obs_on_resume')
+            .then((r: SimpleObservationModel) => {
+              if (r) {
+                if (r.img_url == undefined) {
+                  let a = [];
+                  a.push(event.pendingResult.result);
+                  r.img_url = JSON.stringify(a);
+                } else {
+                  let a = JSON.parse(r.img_url);
+                  a.push(event.pendingResult.result);
+                  r.img_url = JSON.stringify(a);
+                }
+                this.onResumeObs = r;
+              }
+            });
+        }
+      })
     }
 
-    initializeApp() {
+    this.platform.ready().then(() => {
+      let deviceLang = window.navigator.language.substring(0, 2);
 
-        if (this.platform.is('android')) {
-            this.platform.resume.subscribe((event: any) => {
-                if (event && event.pendingResult.pluginServiceName == 'Camera' && event.pendingResult.pluginStatus == 'OK') {
-                    this.s.get('obs_on_resume')
-                        .then((r: SimpleObservationModel) => {
-                            if (r) {
-                                if (r.img_url == undefined) {
-                                    let a = [];
-                                    a.push(event.pendingResult.result);
-                                    r.img_url = JSON.stringify(a);
-                                } else {
-                                    let a = JSON.parse(r.img_url);
-                                    a.push(event.pendingResult.result);
-                                    r.img_url = JSON.stringify(a);
-                                }
-                                this.onResumeObs = r;
-                            }
-                        });
-                }
-            })
+      /**
+       * La langue de l'application (fr, de, it) est sélectionnée sur la base de la langue du téléphone. Si la langue est autre (en par exemple),
+       * c'est le français qui est défini par défaut. Ce choix peut être modifié dans les paramètres. Le module pour les langues est @ngx-translate.
+       */
+      this.s.get('lang').then((val) => {
+        if (val) {
+          this.ts.setDefaultLang(val);
+          this.ts.use(val);
+        } else {
+          if (['fr', 'de', 'it'].indexOf(deviceLang) > -1) {
+            this.s.set('lang', deviceLang);
+            this.ts.setDefaultLang(deviceLang);
+            this.ts.use(deviceLang);
+          } else {
+            this.s.set('lang', 'fr');
+            this.ts.setDefaultLang('fr');
+            this.ts.use('fr');
+          }
         }
 
-        this.platform.ready().then(() => {
-            let deviceLang = window.navigator.language.substring(0, 2);
-
-            this.s.get('lang').then((val) => {
-                if (val) {
-                    this.ts.setDefaultLang(val);
-                    this.ts.use(val);
-                } else {
-                    if (['fr', 'de', 'it'].indexOf(deviceLang) > -1) {
-                        this.s.set('lang', deviceLang);
-                        this.ts.setDefaultLang(deviceLang);
-                        this.ts.use(deviceLang);
-                    } else {
-                        this.s.set('lang', 'fr');
-                        this.ts.setDefaultLang('fr');
-                        this.ts.use('fr');
-                    }
-                }
-
-                if (this.onResumeObs) {
-                    this.s.set('onResumeObs', this.onResumeObs)
-                        .then(() => {
-                            this.rootPage = ImagesPage;
-                        });
-                } else {
-                    this.rootPage = HomeListPage;
-                }
+        /**
+         * Voir plus haut la remarque concernant les photos prises avec un téléphone android. Si l'application redémarre après une prise d'image,
+         * l'utilisateur est renvoyé vers la page d'insertion des images. Sinon, vers la page principal (comportement normal).
+         */
+        if (this.onResumeObs) {
+          this.s.set('onResumeObs', this.onResumeObs)
+            .then(() => {
+              this.rootPage = ImagesPage;
             });
+        } else {
+          this.rootPage = HomeListPage;
+        }
+      });
 
-            if (this.platform.is('ios')) {
-                //this.statusBar.overlaysWebView(false);
-                this.statusBar.backgroundColorByHexString('#f8f8f8')
-                this.statusBar.styleDefault();
-            } else {
-                this.statusBar.styleBlackOpaque();
-            }
+      if (this.platform.is('ios')) {
+        this.statusBar.backgroundColorByHexString('#f8f8f8')
+        this.statusBar.styleDefault();
+      } else {
+        this.statusBar.styleBlackOpaque();
+      }
 
-            this.splashScreen.hide();
+      this.splashScreen.hide();
 
-            let whichInsertPage: any = FirstInsertMapPage;
-            this.s.get('ord')
-                .then((res) => {
-                    if (!res) {
-                        this.s.set('ord', 'map');
-                    }
-                    if (res === 'esp') {
-                        whichInsertPage = FirstSpeciesListPage;
-                    }
-                    this.pages = [
-                        {title: 'INSERTION', component: whichInsertPage},
-                        {title: 'VOSDONNEES', component: HomeListPage},
-                        {title: 'PARAMETRES', component: ParamsPage},
-                        {title: 'CONNECT', component: ConnectPage},
-                        {title: 'ABOUT', component: AboutPage}
-                    ];
-                });
-
-            this.s.get('ord')
-                .then((res) => {
-                    if (!res) this.s.set('ord', 'map');
-                });
-
-            this.s.get('favMode')
-                .then((res) => {
-                    if (!res) this.s.set('favMode', 'most');
-                });
-
-            this.os.checkIfObservationTable();
-            this.ss.checkIfFavSpeciesTable();
-
-            this.s.get('creds')
-                .then((creds) => {
-                    if (creds) {
-                        if (this.n.type == 'none') {
-                            this.s.set('conn', false);
-                        } else {
-                            this.as.login(creds)
-                                .then((r) => {
-                                    if (r.status == 200) {
-                                        this.http.useBasicAuth(creds.email, creds.password);
-                                        this.s.set('conn', true);
-                                    } else if (r.status == 401) {
-                                        this.s.set('conn', false);
-                                    }
-                                })
-                        }
-                    } else {
-                        this.s.set('conn', false);
-                    }
-                });
-
-            this.s.get('selectedGroups')
-                .then((res) => {
-                    if (!res) {
-                        this.s.set('selectedGroups', [true, true, false, false, false, false, true, true, false, true, false, false, false, true, true, true])
-                    }
-                });
-
-            this.s.get('deteGroupSel')
-                .then((s) => {
-                    if (!s) {
-                        this.s.set('deteGroupSel', [false, false, false, false, false, false]);
-                    }
-                });
-
-            this.s.get('lastcoords')
-                .then((res) => {
-                    if (!res) {
-                        this.s.set('lastcoords', [600000, 200000])
-                    }
-                });
+      /**
+       * Initialisation du menu principal (accessible en glissant depuis la gauche). La variable whichInsertPage
+       * permet de fixer si lors de l'insertion d'une nouvelle donnée, c'est la carte (map) ou la liste des espèces (esp)
+       * qui s'affiche.
+       */
+      let whichInsertPage: any = FirstInsertMapPage;
+      this.s.get('ord')
+        .then((res) => {
+          if (!res) {
+            this.s.set('ord', 'map');
+          }
+          if (res === 'esp') {
+            whichInsertPage = FirstSpeciesListPage;
+          }
+          this.pages = [
+            {title: 'INSERTION', component: whichInsertPage},
+            {title: 'VOSDONNEES', component: HomeListPage},
+            {title: 'PARAMETRES', component: ParamsPage},
+            {title: 'CONNECT', component: ConnectPage},
+            {title: 'ABOUT', component: AboutPage}
+          ];
         });
-    }
 
-    openPage(page) {
-        this.nav.setRoot(page.component);
-    }
+      /**
+       * Initialisation du mode d'insertion. map = carte d'abord, esp = liste espèces d'abord. Ces paramètres sont modifiés dans
+       * la page des paramètres.
+       */
+      this.s.get('ord')
+        .then((res) => {
+          if (!res) this.s.set('ord', 'map');
+        });
+
+      /**
+       * Initialisation des tables sqlite contenant les observations et les espèces favorites, si elles n'existent pas déjà.
+       */
+      this.os.checkIfObservationTable();
+      this.ss.checkIfFavSpeciesTable();
+
+      /**
+       * Service d'authentification. Les données sont stockées dans le téléphone. Lors de l'ouverture de l'application, un test est
+       * effectué pour contrôler si l'utilisateur s'est déjà enregistré et s'il a décidé de rester connecté. Si oui, l'app se
+       * reconnecte à l'API.
+       */
+      this.s.get('creds')
+        .then((creds) => {
+          if (creds) {
+            if (this.n.type == 'none') {
+              this.s.set('conn', false);
+            } else {
+              this.as.login(creds)
+                .then((r) => {
+                  if (r.status == 200) {
+                    this.http.useBasicAuth(creds.email, creds.password);
+                    this.s.set('conn', true);
+                  } else if (r.status == 401) {
+                    this.s.set('conn', false);
+                  }
+                })
+            }
+          } else {
+            this.s.set('conn', false);
+          }
+        });
+
+      /**
+       * Initialisation des groupes sélectionnés pour la saisie (espèces s'affichant dans le sous-formulaire des espèces). La liste
+       * sélectionnée est ensuite gardée en mémoire.
+       */
+      this.s.get('selectedGroups')
+        .then((res) => {
+          if (!res) {
+            this.s.set('selectedGroups', [true, true, false, false, false, false, true, true, false, true, false, false, false, true, true, true])
+          }
+        });
+
+      /**
+       * Initialisation des coordonnées (600000/200000 = Berne) pour les cartes. Le dernier endroit visité est ensuite stocké par le module carte.
+       */
+      this.s.get('lastcoords')
+        .then((res) => {
+          if (!res) {
+            this.s.set('lastcoords', [600000, 200000])
+          }
+        });
+    });
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
 }
